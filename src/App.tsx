01709838bd7effa16faaed5d92eda7bf5a6637e3
Roman <roman@muehlfeldner.net>
2020-06-12 15:00:11 +0200

    Develop (#9)
    * Reinitialized amplify

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { ThemeProvider } from './theme/themeContext';
 import GlobalStyles from './globalStyles';
 
-import Amplify from 'aws-amplify';
+import Amplify, { Analytics } from 'aws-amplify';
 import awsConfig from './aws-exports';
 
 Amplify.configure(awsConfig);
+Analytics.configure(awsConfig);
 
 function App({ children }) {
   return (
